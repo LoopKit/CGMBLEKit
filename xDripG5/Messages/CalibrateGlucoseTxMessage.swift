6e24305866e431acd420c9424f113b0238da8aa4
Nathan Racklyeft <loudnate@gmail.com>
2016-03-26 17:57:18 -0400

    Adding more Tx opcodes

---
+++ xDripG5/Messages/CalibrateGlucoseTxMessage.swift
@@ -0,0 +1,14 @@
+//
+//  CalibrateGlucoseTxMessage.swift
+//  xDripG5
+//
+//  Created by Nathan Racklyeft on 3/26/16.
+//  Copyright © 2016 Nathan Racklyeft. All rights reserved.
+//
+
+import Foundation
+
+
+struct CalibrateGlucoseTxMessage {
+    let opcode: UInt8 = 0x34
+}
