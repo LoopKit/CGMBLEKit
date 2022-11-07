4a69ea5327ef1915ec8cfbb17a2e0abecaed678e
Paul Murphy <paul@paultmurphy.com>
2022-11-06 19:30:56 -0700

    Moved 10 day calculation
    Moved the 10 day calculation into the transmitter rx message to keep it away from the view

--- CGMBLEKit/Messages/TransmitterTimeRxMessage.swift
+++ CGMBLEKit/Messages/TransmitterTimeRxMessage.swift
@@ -13,6 +13,7 @@ struct TransmitterTimeRxMessage: TransmitterRxMessage {
     let status: UInt8
     let currentTime: UInt32
     let sessionStartTime: UInt32
+    let sessionExpTime: UInt32
 
     init?(data: Data) {
         guard data.count == 16 && data.isCRCValid else {
@@ -26,6 +27,8 @@ struct TransmitterTimeRxMessage: TransmitterRxMessage {
         status = data[1]
         currentTime = data[2..<6].toInt()
         sessionStartTime = data[6..<10].toInt()
+        sessionExpTime = sessionStartTime + (10*24*60*60)
+
     }
 }
 
